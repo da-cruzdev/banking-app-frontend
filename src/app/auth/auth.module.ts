974be2ee5f8556967eb15a314a7fb15272ff923c
Da-cruz <wilfrieddacruz@arolitec.com>
2023-06-13 12:13:39 +0000

    create login component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from './register/register.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [RegisterComponent],
+  declarations: [RegisterComponent, LoginComponent],
   imports: [CommonModule, AuthRoutingModule, SharedModule],
-  exports: [RegisterComponent],
+  exports: [RegisterComponent, LoginComponent],
 })
 export class AuthModule {}
