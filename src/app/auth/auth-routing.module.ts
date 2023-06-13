2fb9e59816c5b31356f6c25668ae751196868f03
Da-cruz <wilfrieddacruz@arolitec.com>
2023-06-13 15:09:06 +0000

    add form and route for forgot password component

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'account/forgot-password',
+    component: ForgotPasswordComponent,
   },
 ];
 
--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent {
+  forgotPassForm!: FormGroup;
+  responseMessage!: any;
+  hidePassword: boolean = true;
 
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.forgotPassForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    const formData = this.forgotPassForm.value;
+    const data = {
+      email: formData.email,
+      password: formData.password,
+    };
+
+    this.authService.login(data).subscribe();
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  public isControlInvalidAndUntouched(
+    controlName: string,
+    errorKey?: string
+  ): boolean {
+    const control = this.forgotPassForm.get(controlName);
+    const errors = control?.errors;
+
+    return (
+      control?.touched &&
+      control?.dirty &&
+      control?.invalid &&
+      (errorKey ? errors?.[errorKey] : true)
+    );
+  }
 }
